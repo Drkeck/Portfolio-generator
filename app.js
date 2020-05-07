588798d913867aa4ecef60c3f17a357dc17f363a
Drkeck <k3ckl3y.a@gmail.com>
2020-05-06 23:26:10 -0700

    added modular code, and a basic html builder

--- app.js
+++ app.js
@@ -1,11 +1,20 @@
+const fs = require("fs");
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.lenght);
+const [name, github] = profileDataArgs;
+// const printProfileData = profileDataArr => {
+//     for (let i = 0; i < profileDataArr.length; i += 1) {
+//         console.log(profileDataArr[i]);
+//     }
+//     console.log('==============================');
+//     profileDataArr.forEach(profileItem => console.log(profileItem));
+// }
 
-const printProfileData = profileDataArr => {
-    for (let i = 0; i < profileDataArr.length; i += 1) {
-        console.log(profileDataArr[i]);
-    }
-    console.log('==============================');
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-}
+// printProfileData(profileDataArgs);
 
-printProfileData(profileDataArgs);
+
+fs.writeFile('index.html', generatePage(name,github), err => {
+    if (err) throw err;
+
+    console.log('portfolio complete! Check out index.html to see the output!');
+});
