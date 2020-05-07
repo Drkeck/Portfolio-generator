ca6da614b74d7a1e267492f26ce261495f81eda9
Drkeck <k3ckl3y.a@gmail.com>
2020-05-07 05:47:28 -0700

    added inqurer, and learned more aobut node js

--- app.js
+++ app.js
@@ -1,20 +1,19 @@
-const fs = require("fs");
-const generatePage = require('./src/page-template.js');
+const fs = require("fs"); //file system
+const inquirer = require("inquirer"); //npm package
+const generatePage = require('./src/page-template.js'); //site builder
 const profileDataArgs = process.argv.slice(2, process.argv.lenght);
 const [name, github] = profileDataArgs;
-// const printProfileData = profileDataArr => {
-//     for (let i = 0; i < profileDataArr.length; i += 1) {
-//         console.log(profileDataArr[i]);
-//     }
-//     console.log('==============================');
-//     profileDataArr.forEach(profileItem => console.log(profileItem));
-// }
 
-// printProfileData(profileDataArgs);
-
-
-fs.writeFile('index.html', generatePage(name,github), err => {
-    if (err) throw err;
-
-    console.log('portfolio complete! Check out index.html to see the output!');
-});
+inquirer
+    .prompt([
+    {
+        type: 'input',
+        name: 'name',
+        message: 'what is your name?'
+    }
+    ])
+    .then(answers => console.log(answers));
+// fs.writeFile('index.html', generatePage(name,github), err => {
+//     if (err) throw err;
+//     console.log('portfolio complete! Check out index.html to see the output!');
+// });
